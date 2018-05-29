3fda5f30dd92c874ec3883af4739f599b76090f8
Jiguan Ema <Jiguan Ema@DESKTOP-8EJQ2TV>
2018-05-29 09:42:31 -0600

    Agragacion de primeras clases

---
+++ Proyecto_final/Productos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_final
+{
+    class Productos
+    {
+        int codigo_producto;
+        string nombre_producto;
+        int cantidad_producto;
+        int precio_producto;
+
+        public int Codigo_producto { get => codigo_producto; set => codigo_producto = value; }
+        public string Nombre_producto { get => nombre_producto; set => nombre_producto = value; }
+        public int Cantidad_producto { get => cantidad_producto; set => cantidad_producto = value; }
+        public int Precio_producto { get => precio_producto; set => precio_producto = value; }
+    }
+}
---
+++ Proyecto_final/Ventas_clase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_final
+{
+    class Ventas_clase
+    {
+        string nombre_de_vendedor;
+        string nombre_producto;
+        int codigo_producto;
+
+    }
+}
